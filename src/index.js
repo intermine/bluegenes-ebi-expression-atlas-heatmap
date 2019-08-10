2e8af84b0652a2be1241399eb596a4dfcfa368a3
akshat <akshatbhargavadeepa@gmail.com>
2019-08-10 20:36:59 +0530

    added ebi-heatmap lib example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-// add any imports if needed, or write your script directly in this file.
-// const SomePackage = require('PackageName');
+const expressionAtlasHeatmap = require('expression-atlas-heatmap-highcharts');
 
 // make sure to export main, with the signature
 function main(el, service, imEntity, state, config) {
-  if (!state) state = {};
+	if (!state) state = {};
 	if (!el || !service || !imEntity || !state || !config) {
 		throw new Error('Call main with correct signature');
 	}
@@ -25,11 +24,19 @@ function main(el, service, imEntity, state, config) {
     });
 
   */
-	el.innerHTML = `
-		<div class="rootContainer">
-			<h1>Your Data Viz Here</h1>
-		</div>
-	`;
+
+	expressionAtlasHeatmap.render({
+		query: {
+			gene: 'ENSG00000066279'
+		},
+		target: 'yourDiv'
+	});
+
+	// el.innerHTML = `
+	// 	<div class="rootContainer">
+	// 		<h1>Your Data Viz Here</h1>
+	// 	</div>
+	// `;
 }
 
 module.exports = { main };
