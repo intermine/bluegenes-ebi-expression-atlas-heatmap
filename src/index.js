cc737f679d13738d7a06f0c726f2605c405137f4
akshat <akshatbhargavadeepa@gmail.com>
2019-08-26 00:40:15 +0530

    tried integrating via online scripts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-// var expressionAtlasHeatmap = require('expression-atlas-heatmap-highcharts');
-
-/* eslint-ignore */
-
 // make sure to export main, with the signature
 function main(el, service, imEntity, state, config) {
 	if (!state) state = {};
@@ -9,18 +5,23 @@ function main(el, service, imEntity, state, config) {
 		throw new Error('Call main with correct signature');
 	}
 
-	expressionAtlasHeatmapHighcharts.render({
-		query: {
-			gene: 'ENSG00000066279'
-		},
-		target: 'yourDiv'
-	});
+	const script1 = document.createElement('script');
+	script1.src =
+		'https://www.ebi.ac.uk/gxa/resources/js-bundles/vendorCommons.bundle.js';
+	const script2 = document.createElement('script');
+	script2.src =
+		'https://www.ebi.ac.uk/gxa/resources/js-bundles/expressionAtlasHeatmapHighcharts.bundle.js';
+	document.head.appendChild(script1);
+	document.head.appendChild(script2);
 
-	// el.innerHTML = `
-	// 	<div class="rootContainer">
-	// 		<h1>Your Data Viz Here</h1>
-	// 	</div>
-	// `;
+	setTimeout(() => {
+		expressionAtlasHeatmapHighcharts.render({
+			query: {
+				gene: 'ENSG00000066279' || imEntity.value
+			},
+			target: el.id
+		});
+	}, 1000);
 }
 
 module.exports = { main };
